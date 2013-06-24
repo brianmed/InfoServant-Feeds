55bab3eae8f13f8e596eb0acd0926aaf5bfdceba
Brian Medley <pub-bitbucket@bmedley.org>
2013-06-24 15:52:31 +0000

    Automate backups

--- bin/backup_db.sh
+++ bin/backup_db.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-cd /opt/infoservant.com/sql && sudo -u postgres /usr/pgsql-9.2/bin/pg_dump scotch_egg -f scotch_egg.txt.$(date "+%FT%T")
+set -e
+
+cd /opt/infoservant.com/sql
+
+FILE=scotch_egg.txt.$(date "+%FT%T")
+sudo -u postgres /usr/pgsql-9.2/bin/pg_dump scotch_egg -f $FILE
+gzip $FILE
+
+# Keep three days of backups
+ls scotch_egg.txt.201* | grep -v -f <(ls scotch_egg.txt.201* | sort | tail -72) | xargs -r rm
